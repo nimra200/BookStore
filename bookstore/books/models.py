58580c122ef41b3aeb573247ea53c0fc0f316ba2
Nimra Aftab <nimra.aftab@mail.utoronto.ca>
2022-09-22 14:28:33 -0400

    create a book

--- bookstore/books/models.py
+++ bookstore/books/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Book(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=200, unique=True)
     author = models.CharField(max_length=200)
     publication_year = models.PositiveIntegerField()
     price = models.DecimalField(decimal_places=2, max_digits=5)
--- bookstore/books/views.py
+++ bookstore/books/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework.response import Response 
+from .models import Book
+from .serializers import BookSerializer
+from rest_framework.decorators import api_view
 
 # Create your views here.
+@api_view(['POST'])
+def bookCreate(request):
+    serializer = BookSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
