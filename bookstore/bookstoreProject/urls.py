a17e1fda624aeb9514d6f2241ebaec68d85baec8
Ralph <ralph@notreal.com.nothing>
2022-09-23 19:41:44 +0200

    added auth

--- bookstore/bookstoreProject/urls.py
+++ bookstore/bookstoreProject/urls.py
@@ -15,17 +15,21 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth.models import User
 from rest_framework.routers import DefaultRouter
 from rest_framework import routers
-from books.views import BookViewSet, BookStoreViewSet
+from rest_framework.authtoken.views import obtain_auth_token
+from books.views import BookViewSet, BookStoreViewSet, UserViewSet
 
 # Wire up our API using automatic URL routing.
 router = DefaultRouter()
+router.register(r'api/users', UserViewSet, basename='user')
 router.register(r'api/books', BookViewSet, basename='book')
 router.register(r'api/bookstores', BookStoreViewSet, basename='bookstore')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path(r'', include(router.urls)),
-    path(r'api/', include('rest_framework.urls', namespace='rest_framework'))
+    path(r'api/', include('rest_framework.urls', namespace='rest_framework')),
+    path('api-token-auth/', obtain_auth_token, name='api-token-auth')
 ]
